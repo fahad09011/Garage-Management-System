04a253bc33bcc1e61faf89534afb2c0580f49326
fahad09011 <fahadimran038@gmail.com>
2025-02-01 15:04:10 +0000

    Your branch is up to date with 'origin/main'. 	modified:   AdminDashboard.html 	modified:   Assets/css/Admindashboard.css 	modified:   Assets/js/admin_dashboard.js

--- Assets/js/admin_dashboard.js
+++ Assets/js/admin_dashboard.js
@@ -1,29 +1,58 @@
 let container = document.getElementById("container");
-var chart=new ej.charts.Chart({
-    width: '150px',
-    height: '150px',
-//InitializingPrimaryXAxis
-primaryXAxis:{
-valueType:'Category',
-// title:'Countries',
-},
- //InitializingPrimaryYAxis
- primaryYAxis:{
-title:'Medals in number'
-},
+function function_to_create_chart(graph_containerid,datasours,title,name_of_graph){
 
-//InitializingChartSeries
-series:[
-{
-type:'Column',
-dataSource:[
-{country:"x",medal:50},
-{country:"y",medal:40},
-{country:"z",medal:70},
-],
-xName:'country',
-yName:'medal',
+    var chart=new ej.charts.Chart({
+        background: 'none',
+        width: '190px',
+        height: '120px',
+    
+    primaryXAxis:{
+    valueType:'Category',
+    
+    },
+     
+     primaryYAxis:{
+    
+    },
+    
+    
+    series:[
+    {
+    type:'Column',
+    dataSource:datasours,
+    
+    xName:name_of_graph,
+    yName:'medal',
+    }
+    ],
+    title:title,
+    });
+    chart.appendTo(graph_containerid);
+    
 }
-],
-});
-chart.appendTo('#container');
+
+let datasource1 =[
+    {income:"1", medal:20},
+    {income:"2", medal:40},
+    {income:"3", medal:20},
+    {income:"4", medal:10},
+    {income:"5", medal:40}
+];
+let datasource2 =[
+    {customer:"1", medal:5},
+    {customer:"2", medal:15},
+    {customer:"3", medal:19},
+    {customer:"4", medal:30},
+    {customer:"5", medal:26}
+];
+let datasource3 =[
+    {delivery:"1", medal:5},
+    {delivery:"2", medal:15},
+    {delivery:"3", medal:9},
+    {delivery:"4", medal:30},
+    {delivery:"5", medal:9}
+];
+function_to_create_chart('#graph_container1',datasource1,'Weekly income','income');
+function_to_create_chart('#graph_container2',datasource2,'Weekly customer','customer');
+function_to_create_chart('#graph_container3',datasource3,'Weekly Deliveries','delivery');
+
